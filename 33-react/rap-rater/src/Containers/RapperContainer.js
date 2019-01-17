5217dd83f6e5125c9b2bc3d1023b1b78cb57f2cd
github username <github email address>
2019-01-17 14:14:26 -0500

    added comments

--- 33-react/rap-rater/src/Containers/RapperContainer.js
+++ 33-react/rap-rater/src/Containers/RapperContainer.js
@@ -13,14 +13,19 @@ const RapperContainer = props => {
   ));
   return (
     <div>
+      {/*Why use Switch? Because we want the RapperContainer to be responsible for any paths attached to the /rapper index.*/}
       <Switch>
         <Route
           path="/rappers/:name"
           render={routerProps => {
+            //Recieve props from Route, find the rapper's name by going into the Route's props and finding the name that was entered into the URL
+            //Something to note: the only reason we can find the name using "match.params.name" is because in our path we use ":name". For example, if in our path we had our dynamic route written as :butt we would have to find the rapper's name by calling "match.params.butt". You're welcome Skyler
             let rapperName = routerProps.match.params.name;
+            //use that name and find the rapper in our rapperList who has that name
             let rapper = props.rapperList.find(rapper =>
               rapper.name.includes(rapperName)
             );
+            //Return a <RapperCard /> who's "rapper" prop evaluates to the rapper who's name matched the name entered in our URL
             return (
               <RapperCard
                 rapper={rapper}
@@ -30,6 +35,7 @@ const RapperContainer = props => {
             );
           }}
         />
+        {/*If router matches "/rappers" simply render our array of <RapperCard/>'s */}
         <Route
           path="/rappers"
           render={() => {
